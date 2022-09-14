c5983bede989b816570726ec8b0a5280e41d3b34
Vladimir Radanov <vladimir.radanov@gmail.com>
2022-09-14 12:14:50 +0300

    Explicit waiter

--- src/main/java/selenium/BaseWebTest.java
+++ src/main/java/selenium/BaseWebTest.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -89,13 +92,10 @@ public class BaseWebTest {
 
     public void checkEmailAccount(String login) {
 //        webDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-//        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
-        Utilities.treadWaiter(2);
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
+//        Utilities.treadWaiter(2);
         switchToLoginFrame();
-
-        /*WebElement loggedEmailElement = wait.until(ExpectedConditions.
-                visibilityOf(webDriver.findElement(By.xpath(loggedEmailXPath))));*/
-
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(emailAccountXPath)));
         String loggedEmail = webDriver.findElement(By.xpath(emailAccountXPath)).getText();
 
         if ((login + "@ukr.net").equals(loggedEmail)) System.out.println("Email account is CORRECT:\t" + loggedEmail);
